6aee980589390bd9e5e45043fb0e3eab32a4454c
Vishal Sanserwal <vsanserwal@sapient.com>
2019-10-10 02:02:54 +0530

    updated

--- js/hr.js
+++ js/hr.js
@@ -2,6 +2,7 @@
 
 const hr = {
     showCandidates:function(){
+        $("#interview_candidates").empty();
         firebase.database().ref(`candidates/`)
         .once("value", (snapshot)=>{
             var candidates = snapshot.val();
@@ -13,8 +14,8 @@ const hr = {
                         <h5 class="card-title">${candidates[candidate].name}</h5>
                         <p class="card-text">Skills: ${candidates[candidate].skills.join(",")}</p>
                         <p class="card-text text-secondary">Interview at: ${candidates[candidate].interviewDate}</p>
-                        <a href="#" class="btn btn-primary">Selected</a>
-                        <a href="#" class="btn btn-danger">Rejected</a>
+                        <a href="javascript:void(0)" onclick="hr.selectCandidate(${candidate})" class="btn btn-primary">Selected</a>
+                        <a href="javascript:void(0)" onclick="hr.rejectCandidate(${candidate})" class="btn btn-danger">Rejected</a>
                     </div>
                     </div>
                 </div>`
@@ -50,5 +51,40 @@ const hr = {
         $(".interview_card").removeClass("active");
         $(`#${event.target.id}`).addClass("active");
         $(`.${event.target.id}`).addClass("active");
+    },
+    addCandidate: function(){
+        var cid = new Date().getTime();
+        var cData = {
+            name: $("#addCandidate-name").val(),
+            skills: $("#addCandidate-skills").val().split(","),
+            interviewDate: $("#addCandidate-date").val(),
+            status: "scheduled",
+            createdAt: cid * -1
+        }
+        firebase.database().ref(`candidates/${cid}`)
+        .set(cData);
+        var candidateUI = `<div class="col-sm-6 interview_card interview_filter_schd active p-2">
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${cData.name}</h5>
+                        <p class="card-text">Skills: ${cData.skills.join(",")}</p>
+                        <p class="card-text text-secondary">Interview at: ${cData.interviewDate}</p>
+                        <a href="javascript:void(0)" onclick="hr.selectCandidate(${cid})" class="btn btn-primary">Selected</a>
+                        <a href="javascript:void(0)" onclick="hr.rejectCandidate(${cid})" class="btn btn-danger">Rejected</a>
+                    </div>
+                    </div>
+                </div>`
+        $("#interview_candidates").prepend(candidateUI);
+        $("#addCandidate .close").trigger("click");
+    },
+    selectCandidate: function(uid){
+        firebase.database().ref(`candidates/${uid}/status`)
+        .set("selected");
+        this.showCandidates();
+    },
+    rejectCandidate: function(uid){
+        firebase.database().ref(`candidates/${uid}/status`)
+        .set("rejected");
+        this.showCandidates();
     }
 }
