82ea1039ececa51f72c40167e5fd3049a73e6ae0
gmehta3 <gmehta2@sapient.com>
2019-10-09 20:06:45 +0530

    Added authentication for only publicisgroupe users only

--- js/build/bundle.js
+++ js/build/bundle.js
@@ -40,6 +40,10 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
+    if (!user.email.split("@")[1] == "publicisgroupe.net") {
+      auth.logout();
+    }
+
     $("#login").hide();
     $("#myprofile, #logout").show();
     localStorage.setItem(config.isloggedin, "true");
