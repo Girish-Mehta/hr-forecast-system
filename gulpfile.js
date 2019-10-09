91e00a19cb273670603855f6ca5e6d47dbb6b927
Vishal Sanserwal <vsanserwal@sapient.com>
2019-10-09 12:32:09 +0530

    added jquery and somw

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('scss', function () {
-    return gulp.src('./renderer/css/main.scss')
+    return gulp.src('./scss/main.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./renderer/css/'));
+    .pipe(gulp.dest('./scss/'));
 });
 
 gulp.task('default',['scss'], function () {
-    gulp.watch('./renderer/css/*.scss', ['scss']);
+    gulp.watch('./scss/*.scss', ['scss']);
 });
