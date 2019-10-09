8e8f48c0f1ccbbf01b57d7d588da55ca3d6ca7ab
Vishal Sanserwal <vsanserwal@sapient.com>
2019-10-09 13:52:02 +0530

    added netlify

--- js/build/bundle.js
+++ js/build/bundle.js
@@ -24,3 +24,40 @@ function handleShowLogin() {
 function handleShowRegister() {
   auth.showRegister();
 }
+
+window.onload = function () {
+  handleUrl(location.href);
+};
+
+function handleUrl(url) {
+  var params = getUrlParams(url);
+
+  if (Object.keys(params).length == 0) {
+    var urlParts = url.split("/");
+    var navigate = urlParts.pop();
+    var urlToOpen = urlRedirect[navigate];
+
+    if (url.includes("template")) {
+      urlRedirect["userCv"](urlParts[urlParts.length - 1], navigate);
+    } else if (urlRedirect[navigate]) {
+      urlToOpen();
+    } else {
+      urlRedirect.landing();
+    }
+  } else {
+    urlRedirect[params.mode](params.oobCode);
+  }
+}
+
+function getUrlParams(url) {
+  var params = {};
+  var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    params[key] = value;
+  });
+  return params;
+}
+
+var urlRedirect = {
+  login: auth.showLogin,
+  register: auth.showRegister
+};
