64d9a4dc310ed376d8f136587b689f739b203ce1
gmehta3 <gmehta2@sapient.com>
2019-10-10 00:55:50 +0530

    Modified a lot of css and dom for styling of PM dashboard

--- js/build/bundle.js
+++ js/build/bundle.js
@@ -48,7 +48,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     $("#login").hide();
     $("#myprofile, #logout").show();
     localStorage.setItem(config.isloggedin, "true");
-    $("#myprofile button").text(user.displayName);
+    $("#myprofile .user-name").text(user.displayName);
     showDashboard();
   } else {
     $("#login").show();
@@ -172,22 +172,36 @@ var pm = {
     this.getTeamName().then(function (pm) {
       firebase.database().ref("accounts/".concat(pm.val().project, "/members")).once("value", function (snapshot) {
         var members = snapshot.val();
+        $("#members").empty();
         Object.keys(members).map(function (key, idx) {
-          var member = "<div class=\"member-container card\"><p class=\"name\">".concat(members[key], "</p></div>");
+          var member = "<div class=\"member-container\"><i class=\"fa fa-user-circle px-2\" aria-hidden=\"true\"></i><p class=\"name\">".concat(members[key], "</p></div>");
           $("#members").append(member);
+          $(".team-members").show();
+          $(".team-requirements").hide();
+          $(".team-title").removeClass("active");
+          $(".team-members-title").addClass("active");
         });
       });
+    });
+  },
+  showRequirements: function showRequirements() {
+    this.getTeamName().then(function (pm) {
       firebase.database().ref("requirements/".concat(pm.val().project, "/")).once("value", function (snapshot) {
         var requirements = snapshot.val();
 
         if (requirements.requirements.length) {
-          $("#team_requirements").append("<h4 class=\"team-members-title\">Requirements</h4>");
-          $("#team_requirements").append("<div id=\"req_total\" class=\"alert alert-primary\" role=\"alert\">\n                    Total requirements: ".concat(requirements.total, "\n                  </div>"));
+          $("#team_requirements").empty();
+          $(".team-requirements #req_total").text(requirements.total);
           requirements.requirements.map(function (requirement) {
             var req = "<div class=\"member-container bg-light\"><p> ".concat(Object.keys(requirement)[0], " ").concat(requirement[Object.keys(requirement)[0]], "</p></div>");
             $("#team_requirements").append(req);
           });
         }
+
+        $(".team-members").hide();
+        $(".team-requirements").show();
+        $(".team-title").removeClass("active");
+        $(".team-requirements-title").addClass("active");
       });
     });
   }
